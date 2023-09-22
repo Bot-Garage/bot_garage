c9ad54c16dfc1e61f5743c96f0b042cca8d90253
johndeshano <4898712+johndeshano@users.noreply.github.com>
2023-09-22 09:57:20 -0400

    Refactor auth_check and api_auth_check functions to include role-based authorization
    - Updated auth_check and api_auth_check functions in _utils.js to include a role parameter
    - If the role is "admin" and the user is not an admin, display an error message
    - The updated functions now check the role of the user before proceeding with the authentication process

--- routes/_utils.js
+++ routes/_utils.js
@@ -1,6 +1,6 @@
 const User = require("../models/user.js");
 
-function auth_check(){
+function auth_check(role){
     return async function(req, res, next){
         // Failure: User doesn't exist in session
         if(!req.session.user || !req.session.user._id){
@@ -9,19 +9,23 @@ function auth_check(){
         }
         
         // Failure: User not found in database
-        var foundUser = await User.findById(req.session.user._id).exec();
-        if(!foundUser){
+        var db_user = await User.findById(req.session.user._id).exec();
+        if(!db_user){
             req.session.user = null;
             return res.redirect("/login/");
         }
 
+        if(role == "admin" && db_user.admin == false){
+            return res.render("error", { message: "Invalid permissions to view this page." });
+        }
+
         // Success
-        req.session.user = foundUser;
+        req.session.user = db_user;
         next();
     }
 }
 
-function api_auth_check(){
+function api_auth_check(role){
     return async function(req, res, next){
         // Failure: User doesn't exist in session
         if(!req.session.user || !req.session.user._id){
@@ -30,14 +34,18 @@ function api_auth_check(){
         }
         
         // Failure: User not found in database
-        var foundUser = await User.findById(req.session.user._id).exec();
-        if(!foundUser){
+        var db_user = await User.findById(req.session.user._id).exec();
+        if(!db_user){
             req.session.user = null;
             return res.send({ success: false, message: "Authentication failed - invalid authentication attempt" });
         }
 
+        if(role == "admin" && db_user.admin == false){
+            return res.send({ success: false, message: "You do not have permission to complete this action." })
+        }
+
         // Success
-        req.session.user = foundUser;
+        req.session.user = db_user;
         next();
     }
 }
